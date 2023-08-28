ec3d466fa8f88eeda24dd19978d7f689533beb38
Pouya Hallaj Zavareh <40316639+Pouyaexe@users.noreply.github.com>
2023-08-28 10:27:15 +0330

    modified:   main.py added click

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 import mediapipe as mp
-from mouse import move_mouse, click_mouse
+from mouse import move_mouse
 
 # Initialize Mediapipe Hands module
 mp_hands = mp.solutions.hands
@@ -23,25 +23,19 @@ while cap.isOpened():
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = hands.process(frame_rgb)
 
-    click_detected = False
-
     if results.multi_hand_landmarks:
         for landmarks in results.multi_hand_landmarks:
-            # Get the coordinates of index, middle, and thumb fingers
-            index_tip = landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
-            middle_tip = landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
+            # Get the coordinates of thumb and index finger
             thumb_tip = landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
-
-            # Calculate the distance between fingers
-            index_thumb_distance = ((index_tip.x - thumb_tip.x)**2 + (index_tip.y - thumb_tip.y)**2)**0.5
-            index_middle_distance = ((index_tip.x - middle_tip.x)**2 + (index_tip.y - middle_tip.y)**2)**0.5
+            index_tip = landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+            milddle_tip = landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
             
-            # If fingers touch, set click_detected to True
-            if index_thumb_distance < 0.02 and index_middle_distance < 0.02:
-                click_detected = True
+            # Calculate the distance between thumb and index finger tips
+            thumb_index_distance = ((thumb_tip.x - index_tip.x)**2 + (thumb_tip.y - index_tip.y)**2)**0.5
+            index_middle_distance = ((index_tip.x - milddle_tip.x)**2 + (index_tip.y - milddle_tip.y)**2)**0.5
 
             # If the distance is small (pinch gesture), move the mouse cursor
-            if not click_detected:
+            if thumb_index_distance  < 0.05:
                 # Calculate the movement vector
                 dx = thumb_tip.x - index_tip.x
                 dy = thumb_tip.y - index_tip.y
@@ -52,14 +46,13 @@ while cap.isOpened():
 
                 # Map normalized coordinates to screen dimensions
                 move_mouse(thumb_tip.x + dx, thumb_tip.y + dy)
-                
-                # Clear any previous click detection text
-                cv2.putText(frame, "", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            
+                # Registering a click if middle finger touches the index finger
+                if index_middle_distance < 0.05:
+                    # Click the left button
+                    print("Click")
 
-        # Display "Click Detected!" text on the screen if click is detected
-        if click_detected:
-            cv2.putText(frame, "Click Detected!", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            click_mouse()
+            
 
     cv2.imshow("Pinch Gesture Mouse Control", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
